fd3053e6ec20e6b4ef53a3f565fd6fea8a7710bd
Eugen.Friesen <Eugen.Friesen@deutschebahn.com>
2021-02-14 09:03:57 +0100

    Added LogDTOMapperTest.java

--- src/test/java/project/logManager/model/mapper/LogDTOMapperTest.java
+++ src/test/java/project/logManager/model/mapper/LogDTOMapperTest.java
@@ -2,6 +2,13 @@ package project.logManager.model.mapper;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import project.logManager.model.entity.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,13 +16,16 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author - EugenFriesen
  * 14.02.2021
  **/
+
+@ExtendWith(MockitoExtension.class)
 class LogDTOMapperTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    @InjectMocks
+    LogDTOMapper systemUnderTest;
 
     @Test
     void mapLogsToLogDTOs() {
+        List<Log> logs = new ArrayList<>();
+        systemUnderTest.mapLogsToLogDTOs(logs);
     }
 }
