5d1d352890110ea43601cfd4ffc6a43d1b2d1092
dwp3402 <florian.wulfert@dwpbank.de>
2022-01-17 17:21:02 +0100

    ITs UserController

--- src/test/java/project/logManager/controller/UserControllerIT.java
+++ src/test/java/project/logManager/controller/UserControllerIT.java
@@ -1,23 +1,27 @@
 package project.logManager.controller;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import project.logManager.model.entity.Log;
 import project.logManager.model.entity.User;
+import project.logManager.model.repository.LogRepository;
 import project.logManager.model.repository.UserRepository;
 
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -26,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureDataJpa
 @ComponentScan(basePackages = {"project.logManager"})
 @Transactional
+@TestPropertySource("/application-test.properties")
 class UserControllerIT {
 
     @Autowired
@@ -34,9 +39,147 @@ class UserControllerIT {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LogRepository logRepository;
+
+    User petra;
+    User torsten;
+    User hans;
+
+    @BeforeAll
+    void setup() {
+        petra = createUser("Petra");
+        torsten = createUser("Torsten");
+        hans = createUser("Hans");
+    }
+
+    @Test
+    void testAddUser() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "05.11.1995")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "blau"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Assertions.assertEquals("User Hugo wurde erstellt. Der User hat einen BMI von 24.07 " +
+                "und ist somit normalgewichtig.", result.getResponse().getContentAsString());
+    }
+
+    // TODO : User kann nicht angelegt werden, da noch keine User in der Datenbank...
+
+    @Test
+    void testActorIsMissingAtAddUser() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "05.11.1995")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "blau"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+
+        Assertions.assertEquals("User Petra nicht gefunden", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testParameterIsMissingAtAddUser() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "05.11.1995")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "blau"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        Assertions.assertEquals("Required String parameter 'actor' is not present",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testColorIsBadAtAddUser() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "05.11.1995")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "braun"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+
+        Assertions.assertEquals("Farbe falsch! Wählen Sie eine der folgenden Farben: blau, rot, " +
+                "orange, gelb, schwarz", result.getResponse().getContentAsString());
+    }
+
+    //Required path variable was not found
+    @Test
+    void testWrongDateFormat() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "hallo")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "braun"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void testWrongWeightFormat() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "08.03.2002")
+                        .param("gewicht", "hi")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "braun"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void testWrongHeightFormat() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Hugo")
+                        .param("geburtsdatum", "hallo")
+                        .param("gewicht", "78")
+                        .param("groesse", "hi")
+                        .param("lieblingsfarbe", "braun"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andReturn();
+    }
+
+    @Test
+    void testUserIsAlreadyDefined() throws Exception {
+        MvcResult result = mockMvc.perform(post("/user")
+                        .param("actor", "Petra")
+                        .param("name", "Petra")
+                        .param("geburtsdatum", "05.11.1995")
+                        .param("gewicht", "78")
+                        .param("groesse", "1.80")
+                        .param("lieblingsfarbe", "blau"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+    }
+
     @Test
     void testFindUsers() throws Exception {
-        createUser("Hans");
         mockMvc.perform(get("/users"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -45,12 +188,15 @@ class UserControllerIT {
 
     @Test
     void testFindUserById() throws Exception {
-        createUser("Petra");
         MvcResult result = mockMvc.perform(get("/user/id")
-                .param("id", "1"))
+                        .param("id", "1"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
+
+        Assertions.assertEquals("{\"id\":1,\"name\":\"Petra\",\"geburtsdatum\":\"1999-12-13\"," +
+                        "\"gewicht\":65.0,\"groesse\":1.6,\"lieblingsfarbe\":\"Rot\",\"bmi\":25.39}",
+                result.getResponse().getContentAsString());
     }
 
     @Test
@@ -59,6 +205,7 @@ class UserControllerIT {
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andReturn();
+
         Assertions.assertEquals("Required Integer parameter 'id' is not present",
                 result.getResponse().getContentAsString());
     }
@@ -66,24 +213,25 @@ class UserControllerIT {
     @Test
     void whenIdToFindNotFoundThenReturnNull() throws Exception {
         MvcResult result = mockMvc.perform(get("/user/id")
-                .param("id", "50"))
+                        .param("id", "50"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
+
         Assertions.assertEquals("null", result.getResponse().getContentAsString());
     }
 
-    /*@Test
+    @Test
     void testDeleteUserById() throws Exception {
-        createUser("Hans");
-        createUser("Petra");
         MvcResult result = mockMvc.perform(delete("/user/delete/1")
                 .param("actor", "Hans"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
-    }*/
+        Assertions.assertEquals("User mit der ID 1 wurde gelöscht!",
+                result.getResponse().getContentAsString());
+    }
 
     @Test
     void whenActorForDeleteByIdIsNullThenReturnBadRequest() throws Exception {
@@ -98,7 +246,6 @@ class UserControllerIT {
 
     @Test
     void whenIdForDeleteIsFalseThenThrowRuntimeException() throws Exception {
-        createUser("Torsten");
         MvcResult result = mockMvc.perform(delete("/user/delete/8")
                         .param("actor", "Torsten"))
                 .andDo(print())
@@ -110,11 +257,18 @@ class UserControllerIT {
     }
 
     @Test
+    void testActorIsTryingToDeleteHimselfId() throws Exception {
+        MvcResult result = mockMvc.perform(delete("/user/delete/2")
+                .param("actor", "Torsten"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+    }
+
+    @Test
     void testDeleteUserByName() throws Exception {
-        createUser("Torsten");
-        createUser("Petra");
         MvcResult result = mockMvc.perform(delete("/user/delete/name/Petra")
-                    .param("actor", "Torsten"))
+                        .param("actor", "Torsten"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
@@ -124,8 +278,19 @@ class UserControllerIT {
     }
 
     @Test
+    void testActorIsTryingToDeleteHimselfName() throws Exception {
+        MvcResult result = mockMvc.perform(delete("/user/delete/name/Torsten")
+                        .param("actor", "Torsten"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+
+        Assertions.assertEquals("Ein User kann sich nicht selbst löschen!",
+                result.getResponse().getContentAsString());
+    }
+
+    @Test
     void whenActorForDeleteByNameIsNullThenReturnBadRequest() throws Exception {
-        createUser("Petra");
         MvcResult result = mockMvc.perform(delete("/user/delete/name/Petra"))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
@@ -135,22 +300,21 @@ class UserControllerIT {
                 result.getResponse().getContentAsString());
     }
 
+    //
     @Test
     void whenActorForDeleteByNameNotFoundThenReturnBadRequest() throws Exception {
-        createUser("Petra");
         MvcResult result = mockMvc.perform(delete("/user/delete/name/Petra")
-                        .param("actor", "Peter"))
+                        .param("actor", "Florian"))
                 .andDo(print())
                 .andExpect(status().isInternalServerError())
                 .andReturn();
 
-        Assertions.assertEquals("User mit dem Namen Peter konnte nicht gefunden werden",
+        Assertions.assertEquals("User mit dem Namen Florian konnte nicht gefunden werden",
                 result.getResponse().getContentAsString());
     }
 
     @Test
     void whenUserNameToDeleteNotFoundThenThrowRuntimeException() throws Exception {
-        createUser("Torsten");
         MvcResult result = mockMvc.perform(delete("/user/delete/name/Petra")
                         .param("actor", "Torsten"))
                 .andDo(print())
@@ -163,7 +327,6 @@ class UserControllerIT {
 
     @Test
     void whenUserNameToDeleteIsNullThenThrowRuntimeException() throws Exception {
-        createUser("Torsten");
         MvcResult result = mockMvc.perform(delete("/user/delete/name/")
                         .param("actor", "Torsten"))
                 .andDo(print())
@@ -176,6 +339,7 @@ class UserControllerIT {
 
     @Test
     void testDeleteAll() throws Exception {
+        logRepository.deleteAll();
         MvcResult result = mockMvc.perform(delete("/user/delete"))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -185,8 +349,22 @@ class UserControllerIT {
                 result.getResponse().getContentAsString());
     }
 
+    @Test
+    void whenUserIsUsedSomewhereThenReturnCouldNotDelete() throws Exception {
+        User user = createUser("Petra");
+        logRepository.save(Log.builder().id(1).user(user).message("Test").severity("INFO")
+                .timestamp(LocalDateTime.of(2000,12,12,12,12,12)).build());
+        MvcResult result = mockMvc.perform(delete("/user/delete"))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andReturn();
 
-    private void createUser(String user) {
+        Assertions.assertEquals("User können nicht gelöscht werden, da sie in einer anderen Tabelle " +
+                        "referenziert werden", result.getResponse().getContentAsString());
+    }
+
+
+    private User createUser(String user) {
         switch (user) {
             case "Petra":
                 User petra = User
@@ -197,11 +375,10 @@ class UserControllerIT {
                         .bmi(25.39)
                         .gewicht(65)
                         .groesse(1.60)
-
                         .lieblingsfarbe("Rot")
                         .build();
                 userRepository.save(petra);
-                break;
+                return petra;
             case "Torsten":
                 User torsten = User
                         .builder()
@@ -214,7 +391,7 @@ class UserControllerIT {
                         .lieblingsfarbe("Blau")
                         .build();
                 userRepository.save(torsten);
-                break;
+                return torsten;
             case "Hans":
                 User hans = User
                         .builder()
@@ -227,9 +404,10 @@ class UserControllerIT {
                         .lieblingsfarbe("Rot")
                         .build();
                 userRepository.save(hans);
-                break;
+                return hans;
             default:
                 break;
         }
+        return null;
     }
 }
