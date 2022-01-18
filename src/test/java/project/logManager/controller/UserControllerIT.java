330abf533277c25b35f8ec3bac13f5d26b3a3d34
Elektros <eugen.friesen@gmx.net>
2022-01-18 14:31:32 +0100

    Parametriesierte Tests for getDeleteUserById hinzugefügt

--- src/test/java/project/logManager/controller/UserControllerIT.java
+++ src/test/java/project/logManager/controller/UserControllerIT.java
@@ -1,8 +1,25 @@
 package project.logManager.controller;
 
-import org.junit.jupiter.api.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,18 +28,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import project.logManager.model.entity.Log;
 import project.logManager.model.entity.User;
 import project.logManager.model.repository.LogRepository;
 import project.logManager.model.repository.UserRepository;
-
-import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UserController.class)
@@ -255,114 +265,68 @@ class UserControllerIT {
 
     @Nested
     class FindUserByIdTests {
-        @Test
-        void testFindUserById() throws Exception {
-            MvcResult result = mockMvc.perform(get("/user/id")
-                            .param("id", "1"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
+      @Test
+      void testFindUserById() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/id")
+                .param("id", "1"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
 
-            Assertions.assertEquals("{\"id\":1,\"name\":\"Petra\",\"geburtsdatum\":\"1999-12-13\"," +
-                            "\"gewicht\":65.0,\"groesse\":1.6,\"lieblingsfarbe\":\"Rot\",\"bmi\":25.39}",
-                    result.getResponse().getContentAsString());
+        Assertions.assertEquals("{\"id\":1,\"name\":\"Petra\",\"geburtsdatum\":\"1999-12-13\"," +
+                "\"gewicht\":65.0,\"groesse\":1.6,\"lieblingsfarbe\":\"Rot\",\"bmi\":25.39}",
+            result.getResponse().getContentAsString());
+      }
+
+      @Test
+      void whenIdToFindIsNullThenReturnBadRequest() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/id"))
+            .andDo(print())
+            .andExpect(status().isBadRequest())
+            .andReturn();
+
+        Assertions.assertEquals("Required Integer parameter 'id' is not present",
+            result.getResponse().getContentAsString());
+      }
+
+      @Test
+      void whenIdToFindNotFoundThenReturnNull() throws Exception {
+        MvcResult result = mockMvc.perform(get("/user/id")
+                .param("id", "50"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andReturn();
+
+        Assertions.assertEquals("null", result.getResponse().getContentAsString());
+      }
+    }
+
+    private static Stream<Arguments> getDeleteUserByIdArguments() {
+        return Stream.of(
+            Arguments.of(false, "/user/delete/1", "Hans", status().isOk(), "User mit der ID 1 wurde gelöscht!"),
+            Arguments.of(false, "/user/delete/1", "Paul", status().isInternalServerError(), "User Paul konnte nicht identifiziert werden!"),
+            Arguments.of(false, "/user/delete/1", null, status().isBadRequest(), "Required String parameter 'actor' is not present"),
+            Arguments.of(false, "/user/delete/8", "Torsten", status().isInternalServerError(), "User mit der ID 8 konnte nicht gefunden werden"),
+            Arguments.of(false, "/user/delete/2", "Torsten", status().isInternalServerError(), "Ein User kann sich nicht selbst löschen!"),
+            Arguments.of(true, "/user/delete/1", "Torsten", status().isInternalServerError(),
+                "User Petra kann nicht gelöscht werden, da er in einer anderen Tabelle referenziert wird!")
+        );
+    }
+
+    @ParameterizedTest(name = "{4}")
+    @MethodSource("getDeleteUserByIdArguments")
+    void testGetDeleteUserById(Boolean userIsReferenced, String url, String actor, ResultMatcher status, String message) throws Exception {
+        if (userIsReferenced) {
+            logRepository.save(Log.builder().id(1).user(petra).message("Test").severity("INFO")
+                .timestamp(LocalDateTime.of(2000, 12, 12, 12, 12, 12)).build());
         }
+        MvcResult result = mockMvc.perform(delete(url)
+                  .param("actor", actor))
+              .andDo(print())
+              .andExpect(status)
+              .andReturn();
 
-        @Test
-        void whenIdToFindIsNullThenReturnBadRequest() throws Exception {
-            MvcResult result = mockMvc.perform(get("/user/id"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andReturn();
-
-            Assertions.assertEquals("Required Integer parameter 'id' is not present",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void whenIdToFindNotFoundThenReturnNull() throws Exception {
-            MvcResult result = mockMvc.perform(get("/user/id")
-                            .param("id", "50"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-
-            Assertions.assertEquals("null", result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void testDeleteUserById() throws Exception {
-            MvcResult result = mockMvc.perform(delete("/user/delete/1")
-                            .param("actor", "Hans"))
-                    .andDo(print())
-                    .andExpect(status().isOk())
-                    .andReturn();
-
-            Assertions.assertEquals("User mit der ID 1 wurde gelöscht!",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void whenActorIsFalseThenReturnBadRequest() throws Exception {
-            MvcResult result = mockMvc.perform(delete("/user/delete/1")
-                            .param("actor", "Paul"))
-                    .andDo(print())
-                    .andExpect(status().isInternalServerError())
-                    .andReturn();
-
-            Assertions.assertEquals("User Paul konnte nicht identifiziert werden!",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void whenActorForDeleteByIdIsNullThenReturnBadRequest() throws Exception {
-            MvcResult result = mockMvc.perform(delete("/user/delete/1"))
-                    .andDo(print())
-                    .andExpect(status().isBadRequest())
-                    .andReturn();
-
-            Assertions.assertEquals("Required String parameter 'actor' is not present",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void whenIdForDeleteIsFalseThenThrowRuntimeException() throws Exception {
-            MvcResult result = mockMvc.perform(delete("/user/delete/8")
-                            .param("actor", "Torsten"))
-                    .andDo(print())
-                    .andExpect(status().isInternalServerError())
-                    .andReturn();
-
-            Assertions.assertEquals("User mit der ID 8 konnte nicht gefunden werden",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void testActorIsTryingToDeleteHimselfId() throws Exception {
-            MvcResult result = mockMvc.perform(delete("/user/delete/2")
-                            .param("actor", "Torsten"))
-                    .andDo(print())
-                    .andExpect(status().isInternalServerError())
-                    .andReturn();
-
-            Assertions.assertEquals("Ein User kann sich nicht selbst löschen!",
-                    result.getResponse().getContentAsString());
-        }
-
-        @Test
-        void testUserIdCantBeDeletedBecauseHeIsReferencedSomewhere() throws Exception {
-            User user = createUser("Petra");
-            logRepository.save(Log.builder().id(1).user(user).message("Test").severity("INFO")
-                    .timestamp(LocalDateTime.of(2000,12,12,12,12,12)).build());
-            MvcResult result = mockMvc.perform(delete("/user/delete/1")
-                            .param("actor", "Torsten"))
-                    .andDo(print())
-                    .andExpect(status().isInternalServerError())
-                    .andReturn();
-
-            Assertions.assertEquals("User Petra kann nicht gelöscht werden, da er in einer anderen Tabelle " +
-                    "referenziert wird!", result.getResponse().getContentAsString());
-        }
+        Assertions.assertEquals(message, result.getResponse().getContentAsString());
     }
 
     @Nested
@@ -443,8 +407,7 @@ class UserControllerIT {
 
         @Test
         void whenUserNameIsUsedSomewhereThenReturnCouldNotDelete() throws Exception {
-            User user = createUser("Petra");
-            logRepository.save(Log.builder().id(1).user(user).message("Test").severity("INFO")
+            logRepository.save(Log.builder().id(1).user(petra).message("Test").severity("INFO")
                     .timestamp(LocalDateTime.of(2000,12,12,12,12,12)).build());
             MvcResult result = mockMvc.perform(delete("/user/delete/name/Petra")
                             .param("actor", "Torsten"))
@@ -473,8 +436,7 @@ class UserControllerIT {
 
         @Test
         void whenUserIsUsedSomewhereThenReturnCouldNotDelete() throws Exception {
-            User user = createUser("Petra");
-            logRepository.save(Log.builder().id(1).user(user).message("Test").severity("INFO")
+            logRepository.save(Log.builder().id(1).user(petra).message("Test").severity("INFO")
                     .timestamp(LocalDateTime.of(2000,12,12,12,12,12)).build());
             MvcResult result = mockMvc.perform(delete("/user/delete"))
                     .andDo(print())
