65ff240dc04595f03492ff56dfd47cc989a8751c
Eugen Friesen <eugen.friesen@dwpbank.de>
2022-02-17 17:06:32 +0100

    displayed Columns from backend

--- frontend/logmanager-frontend/src/app/components/user/user.component.ts
+++ frontend/logmanager-frontend/src/app/components/user/user.component.ts
@@ -11,12 +11,6 @@ export interface UserAtributes {
   bmi: number;
 }
 
-const ELEMENT_DATA: UserAtributes[] = [
-  {id: 1, name: 'Peter', birthdate: new Date(2001,12,19).toDateString(), weight: 75, height: 1.80, favouriteColor: "blue", bmi: 23.14},
-  {id: 2, name: 'Hans', birthdate: new Date(2002,2,8).toDateString(), weight: 100, height: 1.80, favouriteColor: "yellow", bmi: 30.86},
-  {id: 3, name: 'Werner', birthdate: new Date(1992,3,24).toDateString(), weight: 80, height: 1.80, favouriteColor: "red", bmi: 24.69},
-];
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -26,12 +20,13 @@ export class UserComponent {
 
   constructor(private userService: UserService) {}
 
+  dataSource: any;
+
   ngOnInit(): void {
     this.userService.getUsers().subscribe(result => {
-      console.log(result);
+      this.dataSource = result.body
     });
   }
 
-  displayedColumns: string[] = ['id', 'name', 'birthdate', 'weight', 'height', 'favouriteColor', 'bmi'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['name', 'birthdate', 'weight', 'height', 'favouriteColor', 'bmi'];
 }
