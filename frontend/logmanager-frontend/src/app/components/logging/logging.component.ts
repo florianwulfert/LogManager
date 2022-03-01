e8cf9306c24b20ca22ddff625c92c602f13d98ba
dwp3402 <florian.wulfert@dwpbank.de>
2022-03-01 17:55:04 +0100

    Added Log Table

--- frontend/logmanager-frontend/src/app/components/logging/logging.component.ts
+++ frontend/logmanager-frontend/src/app/components/logging/logging.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-logging',
@@ -12,4 +13,15 @@ export class LoggingComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  displayedColumns: string[] = ['id', 'message', 'severity', 'timestamp', 'user', 'delete'];
+  listIsEmptyMessage: string = 'There are no logs to show!';
+  dataSource: any;
+
+  public form: FormGroup = new FormGroup({
+    id: new FormControl(),
+    message: new FormControl(),
+    severity: new FormControl(),
+    timestamp: new FormControl(),
+    user: new FormControl()
+  })
 }
