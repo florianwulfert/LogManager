dc9314e000b0faca29fef2334e33d6741ba4007f
dwp3402 <florian.wulfert@dwpbank.de>
2022-03-03 11:50:45 +0100

    Fixed LogControllerTest

--- src/test/java/project/logManager/controller/LogControllerTest.java
+++ src/test/java/project/logManager/controller/LogControllerTest.java
@@ -6,11 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import project.logManager.model.mapper.LogDTOMapper;
 import project.logManager.service.model.LogService;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+
+import static org.mockito.Mockito.verify;
 
 /** @author - EugenFriesen 14.02.2021 */
 @ExtendWith(MockitoExtension.class)
@@ -20,45 +21,42 @@ class LogControllerTest {
 
   @Mock LogService logService;
 
-  @Mock LogDTOMapper logDTOMapper;
-
   @Test
   void testGetLogs() {
     LocalDateTime startDate = LocalDateTime.of(2020, Month.JANUARY, 25, 15, 0, 0);
     LocalDateTime endDate = LocalDateTime.of(2020, Month.JANUARY, 25, 18, 0, 0);
     systemUnderTest.getLogs("INFO", "Test", startDate, endDate);
-    Mockito.verify(logService, Mockito.times(1))
+    verify(logService, Mockito.times(1))
         .getLogs(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
-    Mockito.verify(logDTOMapper, Mockito.times(1)).logsToLogDTOs(Mockito.any());
   }
 
   @Test
   void testAddLog() {
     systemUnderTest.addLog("INFO", "Test", "Peter");
-    Mockito.verify(logService).addLog(Mockito.any(), Mockito.any(), Mockito.any());
+    verify(logService).addLog(Mockito.any(), Mockito.any(), Mockito.any());
   }
 
   @Test
   void testGetLogsById() {
     systemUnderTest.getLogsByID(1);
-    Mockito.verify(logService).searchLogsByID(1);
+    verify(logService).searchLogsByID(1);
   }
 
   @Test
   void testDeleteById() {
     systemUnderTest.deleteLogsByID(1);
-    Mockito.verify(logService).deleteById(1);
+    verify(logService).deleteById(1);
   }
 
   @Test
   void testDeleteBySeverity() {
     systemUnderTest.deleteLogsBySeverity("INFO");
-    Mockito.verify(logService).deleteBySeverity("INFO");
+    verify(logService).deleteBySeverity("INFO");
   }
 
   @Test
   void testDeleteAll() {
     systemUnderTest.deleteAll();
-    Mockito.verify(logService).deleteAll();
+    verify(logService).deleteAll();
   }
 }
