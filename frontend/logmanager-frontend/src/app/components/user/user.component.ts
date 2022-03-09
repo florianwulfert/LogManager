1d1d790f9b07d002ea93ef54f9859711167e3c63
dwp3402 <florian.wulfert@dwpbank.de>
2022-03-09 13:43:27 +0100

    validation for input fields added

--- frontend/logmanager-frontend/src/app/components/user/user.component.ts
+++ frontend/logmanager-frontend/src/app/components/user/user.component.ts
@@ -3,7 +3,7 @@ import {UserFacade} from "../../modules/user/user.facade";
 import {SubscriptionManager} from "../../../assets/utils/subscription.manager";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatSnackBar} from "@angular/material/snack-bar";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AddUserRequest} from "../../modules/user/addUser/dto/add-user-request";
 import {FeatureManager} from "../../../assets/utils/feature.manager";
 
@@ -14,7 +14,8 @@ import {FeatureManager} from "../../../assets/utils/feature.manager";
 })
 export class UserComponent implements OnInit, OnDestroy {
 
-  constructor(private userFacade: UserFacade, private _snackBar: MatSnackBar) {}
+  constructor(private userFacade: UserFacade, private _snackBar: MatSnackBar) {
+  }
 
   subscriptionManager = new SubscriptionManager();
   featureManager = new FeatureManager(this._snackBar);
@@ -38,11 +39,11 @@ export class UserComponent implements OnInit, OnDestroy {
   position = new FormControl('above');
 
   public form: FormGroup = new FormGroup({
-    name: new FormControl(),
-    birthdate: new FormControl(),
-    height: new FormControl(),
-    weight: new FormControl(),
-    favouriteColor: new FormControl()
+    name: new FormControl('', [Validators.required]),
+    birthdate: new FormControl('', [Validators.required]),
+    height: new FormControl('', [Validators.required]),
+    weight: new FormControl('', [Validators.required]),
+    favouriteColor: new FormControl('', [Validators.required])
   })
 
   applyFilter(event: Event) {
