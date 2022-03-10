2bf8ce4c5859cb7ae9a7498b70208ff3a23f0323
haya <haya.hasanagha@dwpbank.de>
2022-03-10 09:08:06 +0100

    Bookentity hinzugefügt

---
+++ src/main/java/project/logManager/model/entity/Book.java
@@ -0,0 +1,28 @@
+package project.logManager.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book {
+    
+    @Column
+    private String titel;
+    @Column
+    private Integer erscheinugsjahr;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+}
