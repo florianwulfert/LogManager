34c43468aa641f024420d8b0a48f4bfc2521665b
Kevin Schröder <kevin.schroeder@dwpbank.de>
2022-03-11 14:07:45 +0100

    verbesserung

--- src/main/java/project/logManager/model/repository/BookRepository.java
+++ src/main/java/project/logManager/model/repository/BookRepository.java
@@ -11,7 +11,6 @@ import java.util.List;
 @Repository
 public interface BookRepository extends JpaRepository <Book,Integer> {
 
-    List<Book> findBooks();
 
 
     List<Book> findByTitel(String titel);
