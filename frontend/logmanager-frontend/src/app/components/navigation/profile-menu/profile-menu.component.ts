b3b65d09952669508fd056e433ef381e19cc5f3c
dwp3402 <florian.wulfert@dwpbank.de>
2022-03-21 17:08:26 +0100

    fixed frontend linting issues

--- frontend/logmanager-frontend/src/app/components/navigation/profile-menu/profile-menu.component.ts
+++ frontend/logmanager-frontend/src/app/components/navigation/profile-menu/profile-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {HeaderComponent} from "../header/header.component";
 import {Profile} from "./profile.interface";
@@ -8,18 +8,14 @@ import {Profile} from "./profile.interface";
   templateUrl: './profile-menu.component.html',
   styleUrls: ['./profile-menu.component.scss']
 })
-export class ProfileMenuComponent implements OnInit {
+export class ProfileMenuComponent {
 
   constructor(
     public dialogRef: MatDialogRef<HeaderComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Profile,
   ) { }
 
-  ngOnInit(): void {
-  }
-
   cancel(): void {
     this.dialogRef.close();
   }
-
 }
