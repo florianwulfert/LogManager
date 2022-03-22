123ae26666bdaa2588c836448d499d3fb35ecbbe
haya <haya.hasanagha@dwpbank.de>
2022-03-22 12:33:36 +0100

    initial commit

---
+++ src/test/java/project/logManager/controller/BookControllerTest.java
@@ -0,0 +1,35 @@
+package project.logManager.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class BookControllerTest {
+    @Test
+    void testAddBook() {
+
+    }
+
+    @Test
+    void testDeleteAll() {
+
+    }
+
+    @Test
+    void testDeleteBooksById() {
+
+    }
+
+    @Test
+    void testDeleteBooksByTitel() {
+
+    }
+
+    @Test
+    void testFindBooksBytitel() {
+
+    }
+
+    @Test
+    void testGetAllBooks() {
+
+    }
+}
