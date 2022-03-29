c7ad125c12ab421438f705204a6ce3dc53eb539e
haya <haya.hasanagha@dwpbank.de>
2022-03-29 15:36:47 +0200

    kleine Fehler bei DeleteByTitel nicht erkenen .

--- src/test/java/project/logManager/controller/BookControllerIT.java
+++ src/test/java/project/logManager/controller/BookControllerIT.java
@@ -1,8 +1,11 @@
 package project.logManager.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
@@ -19,29 +22,39 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.context.TestPropertySource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
 
 import project.logManager.common.message.ErrorMessages;
 import project.logManager.common.message.InfoMessages;
 import project.logManager.common.message.TestMessages;
 import project.logManager.model.entity.Book;
-import project.logManager.model.entity.Log;
+import project.logManager.model.entity.User;
 import project.logManager.model.repository.BookRepository;
-import project.logManager.model.repository.LogRepository;
+import project.logManager.model.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(BookController.class)
+@AutoConfigureDataJpa
+@ComponentScan(basePackages = { "project.logManager" })
+@Transactional
 @TestPropertySource("/application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BookControllerIT {
 
         @Autowired
         private BookRepository bookRepository;
+
+        @Autowired
+        private UserRepository userRepository;
 
         @Autowired
         private MockMvc mockMvc;
@@ -51,52 +64,80 @@ class BookControllerIT {
         @BeforeAll
         public void setup() {
                 books = creatBook();
+                User user = User.builder().name("Torsten").birthdate(LocalDate.now()).height(1.8).weight(90)
+                                .favouriteColor("green").build();
+                userRepository.save(user);
+        }
 
+        @Nested
+        class getAllBooksTest {
+                @Test
+                void testGetBooks() throws Exception {
+                        bookRepository.findAll();
+                        MvcResult result = mockMvc
+                                        .perform(get("/books").param("actor", "Torsten"))
+                                        .andDo(print())
+                                        .andExpect(status().isOk())
+                                        .andReturn();
+
+                }
         }
 
         private static Stream<Arguments> getAddBookArguments() {
                 return Stream.of(
                                 Arguments.of(
-                                                "4",
+                                                "1",
                                                 "haya",
                                                 "1998",
+                                                "Torsten",
                                                 status().isOk(),
-                                                String.format(InfoMessages.BOOK_CREATED, "haya")),
+                                                String.format(TestMessages.HAYA, "haya")),
                                 Arguments.of(
-                                                "3",
+                                                "2",
                                                 "petra",
                                                 "1999",
+                                                "Torsten",
                                                 status().isOk(),
-                                                String.format(InfoMessages.BOOK_CREATED, "petra")),
+                                                String.format(TestMessages.PETRA_BOOK, "petra")),
                                 Arguments.of(
-                                                "-1",
+                                                "3",
                                                 "peter",
                                                 "1988",
-                                                status().isInternalServerError(),
-                                                ErrorMessages.NO_BOOKS_FOUNDS),
+                                                null,
+                                                status().isBadRequest(),
+                                                ErrorMessages.ACTOR_NOT_PRESENT),
                                 Arguments.of(
-                                                "lina",
+                                                "4",
+                                                null,
                                                 "1977",
+                                                "Torsten",
                                                 status().isBadRequest(),
-                                                ErrorMessages.PARAMETER_IS_MISSING),
+                                                ErrorMessages.TITLE_IS_NOT_PRESENT),
                                 Arguments.of(
-                                                "sara",
+                                                "5",
                                                 "omar",
-                                                "1888",
+                                                null,
+                                                "Torsten",
                                                 status().isBadRequest(),
-                                                ErrorMessages.PARAMETER_WRONG_FORMAT));
+                                                ErrorMessages.RELEASE_YEAR_IS_NOT_PRESENT));
         }
 
-        @ParameterizedTest(name = "add")
+        @ParameterizedTest(name = "{1}")
         @MethodSource("getAddBookArguments")
-        void testAddBook(String id, String title, String release_year, String message, ResultMatcher status)
+        void testAddBook(
+                        String id,
+                        String titel,
+                        String erscheinungsjahr,
+                        String actor,
+                        ResultMatcher status,
+                        String message)
                         throws Exception {
+                creatBook();
                 MvcResult result = mockMvc
-                                .perform(
-                                                get("/books")
-                                                                .param("Title", title)
-                                                                .param("ReleaseYear", release_year)
-                                                                .param("ID", id))
+                                .perform(post("/book")
+                                                .param("titel", titel)
+                                                .param("erscheinungsjahr", erscheinungsjahr)
+                                                .param("actor", actor))
                                 .andDo(print())
                                 .andExpect(status)
                                 .andReturn();
@@ -109,25 +150,21 @@ class BookControllerIT {
                 @Test
                 void testFindBookBytitel() throws Exception {
                         MvcResult result = mockMvc
-                                        .perform(
-                                                        get("/book/title")
-                                                                        .param("title", "haya"))
+                                        .perform(get("/searchbook").param("titel", "haya"))
                                         .andDo(print())
                                         .andExpect(status().isOk())
                                         .andReturn();
-
-                        assertEquals(TestMessages.HAYA, result.getResponse().getContentAsString());
                 }
 
                 @Test
-                void whenTitleToFindNotFoundThenReturnNull() throws Exception {
+                void whenTitleToFindNotFoundThenReturnIsNotFound() throws Exception {
                         MvcResult result = mockMvc
-                                        .perform(get("/book/title").param("title", "peter"))
+                                        .perform(get("/book/title").param("title", "christina"))
                                         .andDo(print())
                                         .andExpect(status().isNotFound())
                                         .andReturn();
 
-                        assertEquals("null", result.getResponse().getContentAsString());
+                        assertEquals("", result.getResponse().getContentAsString());
 
                 }
         }
@@ -135,74 +172,146 @@ class BookControllerIT {
         private static Stream<Arguments> getDeleteBookById() {
                 return Stream.of(
                                 Arguments.of(
-                                                "14",
-                                                "omar",
-                                                "1234",
+                                                "petra",
+                                                "/deletebookById/1",
+                                                "Torsten",
                                                 status().isOk(),
-                                                String.format(InfoMessages.BOOK_DELETED_ID, 14)),
+                                                String.format(InfoMessages.BOOK_DELETED_ID, 1)),
                                 Arguments.of(
-                                                "16",
-                                                "haya",
-                                                "1966",
-                                                status().isOk(),
-                                                String.format(InfoMessages.BOOK_DELETED_ID, 12)),
-                                Arguments.of(
-                                                "16",
-                                                "lina",
-                                                "1977",
-                                                status().isInternalServerError(),
-                                                String.format(ErrorMessages.BOOK_NOT_FOUND_ID, 16)),
-                                Arguments.of(
-                                                "6",
                                                 "peter",
-                                                "1977",
-                                                status().isInternalServerError(),
-                                                String.format(ErrorMessages.BOOK_NOT_FOUND_ID, 6)));
+                                                "/deletebookById/4",
+                                                "Torsten",
+                                                status().isOk(),
+                                                String.format(InfoMessages.BOOK_DELETED_ID, 4)),
+                                Arguments.of(
+                                                "haya",
+                                                "/deletebookById/kevin",
+                                                "Torsten",
+                                                status().isBadRequest(),
+                                                String.format(TestMessages.ID_FOR_BOOK_HAS_WRONG_FORMAT)),
+                                Arguments.of(
+                                                "omar",
+                                                "/deletebookById/",
+                                                "Torsten",
+                                                status().isNotFound(),
+                                                ""));
         }
 
-        @ParameterizedTest
+        @ParameterizedTest(name = "{0}")
         @MethodSource("getDeleteBookById")
-        void testDeleteBookById(boolean bookIsReferenced,String actor ,ResultMatcher status , String message)
-        throws Exception{
-                )
+        void testDeleteBookById(
+                        String titel,
+                        String url,
+                        String actor,
+                        ResultMatcher status,
+                        String message)
+                        throws Exception {
+                MvcResult result = mockMvc
+                                .perform(delete(url).param("actor", actor))
+                                .andDo(print())
+                                .andExpect(status)
+                                .andReturn();
+                assertEquals(message, result.getResponse().getContentAsString());
 
+        }
+
+        private static Stream<Arguments> getDeleteBookByTitelArguments() {
+                return Stream.of(
+                                Arguments.of(
+                                                "Book was succussfuly deleted",
+                                                "peter",
+                                                "Torsten",
+                                                status().isOk(),
+                                                String.format(InfoMessages.BOOK_DELETED_TITLE, "peter")),
+                                Arguments.of(
+                                                "Book was succussfuly deleted",
+                                                "omar",
+                                                "Torsten",
+                                                status().isOk(),
+                                                String.format(InfoMessages.BOOK_DELETED_TITLE, "omar")),
+                                Arguments.of(
+                                                "Actor is not present ",
+                                                null,
+                                                "Actor",
+                                                status().isBadRequest(),
+                                                ErrorMessages.TITLE_IS_NOT_PRESENT),
+                                Arguments.of(
+                                                "Book to delete is not in database ",
+                                                "BookoToDeleteIsNotBekannt",
+                                                "Torsten",
+                                                status().isOk(),
+                                                InfoMessages.NO_BOOKS_FOUNDS));
+        }
+
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("getDeleteBookByTitelArguments")
+        void TestDeleteBookByTitel(
+                        String zustand,
+                        String titel,
+                        String actor,
+                        ResultMatcher status,
+                        String message) throws Exception {
+                MvcResult result = mockMvc
+                                .perform(delete("/deletebooksByTitel").param("titel", titel).param("actor", actor))
+                                .andDo(print())
+                                .andExpect(status)
+                                .andReturn();
+
+                assertEquals(message, result.getResponse().getContentAsString());
+
+        }
+
+        @Nested
+        class DeleteAllTest {
+                @Test
+                void testDeleteAll() throws Exception {
+
+                        MvcResult result = mockMvc
+                                        .perform(delete("/allBooksdelete"))
+                                        .andDo(print())
+                                        .andExpect(status().isOk())
+                                        .andReturn();
+
+                        assertEquals(InfoMessages.ALL_BOOKS_DELETED, result.getResponse().getContentAsString());
+
+                }
         }
 
         private List<Book> creatBook() {
 
                 List<Book> books = new ArrayList<>();
                 Book haya = Book.builder()
-                                .id(5)
-                                .erscheinungsjahr(1789)
+                                .id(9)
+                                .erscheinungsjahr(1998)
                                 .titel("haya")
                                 .build();
-                bookRepository.save(haya);
+                bookRepository.saveAndFlush(haya);
 
                 Book petra = Book.builder()
-                                .id(7)
-                                .erscheinungsjahr(1089)
+                                .id(4)
+                                .erscheinungsjahr(1989)
                                 .titel("petra")
                                 .build();
-                bookRepository.save(petra);
+                bookRepository.saveAndFlush(petra);
 
                 Book peter = Book.builder()
-                                .id(9)
-                                .erscheinungsjahr(1589)
+                                .id(10)
+                                .erscheinungsjahr(2010)
                                 .titel("peter")
                                 .build();
-                bookRepository.save(peter);
+                bookRepository.saveAndFlush(peter);
                 Book lina = Book.builder()
                                 .id(12)
-                                .erscheinungsjahr(2002)
+                                .erscheinungsjahr(2009)
                                 .titel("lina")
                                 .build();
-                bookRepository.save(lina);
+                bookRepository.saveAndFlush(lina);
                 Book omar = Book.builder()
-                                .id(12)
+                                .id(13)
                                 .erscheinungsjahr(2002)
                                 .titel("omar")
                                 .build();
-                bookRepository.save(omar);
+                bookRepository.saveAndFlush(omar);
                 books.add(haya);
                 books.add(petra);
                 books.add(peter);
