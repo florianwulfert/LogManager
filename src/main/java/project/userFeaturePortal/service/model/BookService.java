13457a369a1d750226e60f1e43d46954977cbff5
Eugen Friesen <eugen.friesen@dwpbank.de>
2022-05-04 12:09:30 +0200

    Did some refactorings for BookService

--- src/main/java/project/userFeaturePortal/service/model/BookService.java
+++ src/main/java/project/userFeaturePortal/service/model/BookService.java
@@ -19,30 +19,29 @@ import java.util.List;
 public class BookService {
 
   private static final Logger LOGGER = LogManager.getLogger(BookService.class);
-  @Autowired private final BookRepository bookRepository;
+  @Autowired
+  private final BookRepository bookRepository;
   private final LogService logService;
   private final BookValidationService bookValidationService;
 
   public List<Book> addBook(Integer erscheinungsjahr, String titel, String actor) {
     bookValidationService.validateActor(actor);
-    bookValidationService.validateErscheinungsjahrAndTitel(erscheinungsjahr,titel);
+    bookValidationService.validateErscheinungsjahrAndTitel(erscheinungsjahr, titel);
+
     Book book = Book.builder().erscheinungsjahr(erscheinungsjahr).titel(titel).build();
     bookRepository.save(book);
-    logService.addLog(
-        LogRequestDto.builder()
-            .message(String.format("Book %s was added.", titel))
-            .severity("INFO")
-            .user(actor)
-            .build());
+
+    logService.addLog(LogRequestDto.builder()
+        .message(String.format("Book %s was added.", titel)).severity("INFO").user(actor)
+        .build());
     LOGGER.info(String.format(InfoMessages.BOOK_CREATED, titel));
     return bookRepository.findAll();
   }
 
   public List<Book> getAllBooks() {
     List<Book> books = bookRepository.findAll();
-
     List<String> bookNames = new ArrayList<>();
-    for (Book book: books)  {
+    for (Book book : books) {
       bookNames.add(book.getTitel());
     }
     LOGGER.info("Books found: " + bookNames);
@@ -58,43 +57,37 @@ public class BookService {
     bookValidationService.checkIfBookIsReferenced(id);
     bookRepository.deleteById(id);
     logService.addLog(LogRequestDto.builder()
-            .message(String.format(InfoMessages.BOOK_DELETED_ID, id))
-            .severity("WARNING")
-            .user(actor).build());
+        .message(String.format(InfoMessages.BOOK_DELETED_ID, id)).severity("WARNING").user(actor)
+        .build());
     return String.format(InfoMessages.BOOK_DELETED_ID, id);
   }
 
   public String deleteByTitel(String titel, String actor) {
     List<Book> booksToDelete = bookRepository.findByTitel(titel);
+
     if (booksToDelete.isEmpty()) {
-      LOGGER.info(String.format(InfoMessages.NO_BOOKS_FOUNDS, titel));
+      LOGGER.warn(String.format(InfoMessages.NO_BOOKS_FOUNDS, titel));
       return String.format(InfoMessages.NO_BOOKS_FOUNDS, titel);
     }
+
     if (booksToDelete.size() == 1) {
       bookRepository.deleteById(booksToDelete.get(0).getId());
       logService.addLog(LogRequestDto.builder()
-              .message(String.format(InfoMessages.BOOK_DELETED_TITLE, titel))
-              .severity("INFO")
-              .user(actor).build());
+          .message(String.format(InfoMessages.BOOK_DELETED_TITLE, titel)).severity("INFO").user(actor)
+          .build());
       return String.format(InfoMessages.BOOK_DELETED_TITLE, titel);
-    } else {
-      String listString = "";
-      for (Book b : booksToDelete) {
-        if (!listString.equals("")) {
-          listString = listString.concat(", ");
-        }
-        listString = listString.concat("{Titel:").concat(b.getTitel()).concat(", Erscheinungsjahr:")
-            .concat(b.getErscheinungsjahr().toString())
-            .concat(", Id:").concat(b.getId().toString());
-      }
-      LOGGER.info(String.format(InfoMessages.BOOK_CAN_NOT_BE_IDENTIFIED, titel));
-      return String.format(InfoMessages.BOOK_CAN_NOT_BE_IDENTIFIED, titel);
     }
+
+    LOGGER.warn(String.format(InfoMessages.BOOK_CAN_NOT_BE_IDENTIFIED, titel));
+    return String.format(InfoMessages.BOOK_CAN_NOT_BE_IDENTIFIED, titel);
   }
 
   public String deleteBooks() {
     bookRepository.deleteAll();
     LOGGER.info(InfoMessages.ALL_BOOKS_DELETED);
+    logService.addLog(LogRequestDto.builder()
+        .message(InfoMessages.ALL_BOOKS_DELETED).severity("INFO").user(null)
+        .build());
     return InfoMessages.ALL_BOOKS_DELETED;
   }
 }
